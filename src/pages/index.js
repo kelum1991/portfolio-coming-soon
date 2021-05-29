6de39601c1060f118d8eaf472e5d9b076f08d576
Kelum Chathuranga <kelumchathuranga@Kelums-MacBook-Pro.local>
2021-05-29 22:39:25 +0530

    Second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,57 @@
 import * as React from "react"
+//import React, { Component } from 'react';
+import CountDown from 'reactjs-countdown';
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import "./index.css"
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
+  <div className="main-container">
+    <div className="main-container__grid">
+      <div className="main-container__grid-left">
+        <div className="banner-wrapper"></div>
+      </div>
+      <div className="main-container__grid-right">
+        <div className="content-wrapper">
+          <div className="logo">
+            <StaticImage
+              src="../images/logo.png"
+              width={180}
+              quality={100}
+              formats={["AUTO", "WEBP", "AVIF"]}
+              alt="A Gatsby astronaut"
+            />
+          </div>
+          <div className="heading">
+            <h1>COMING SOON</h1>
+          </div>
+          <div className="counter">
+          <CountDown
+                    deadline="July 22, 2021"
+                />
+          </div>
+          <div className="main-desc">
+            <p>We are launching soon please reload after days</p>
+          </div>
+          <div className="social-icons">
+            <p className="social-icons__heading">Stay in touch: </p>
+            <ul className="social-icons__list">
+              <li className="social-icons__list-item">
+                <a href="#"></a>
+              </li>
+              <li className="social-icons__list-item">
+                <a href="#"></a>
+              </li>
+              <li className="social-icons__list-item">
+                <a href="#"></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 )
 
 export default IndexPage
